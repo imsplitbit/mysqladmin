bff508d0e4d577bbd1c1b55c80759c7848d9178e
Daniel Salinas <imsplitbit@gmail.com>
2009-01-20 20:54:39 +0000

    First check in of skeleton code to check replication status

--- lib/mysqladmin/replication.rb
+++ lib/mysqladmin/replication.rb
@@ -1,5 +1,52 @@
 module Mysqladmin
   class Replication
+    include Mysqladmin::Arguments
+    include Mysqladmin::Serialize
     
+    class Status
+      def initialize(args = {})
+        @master = args[:master] || nil
+        @slave = args[:slave] || nil
+        @sbmLimit = args[:sbmLimit] || 600
+      end
+  
+      def repStatusHost(args={})
+        @master = args[:master] unless @master
+        @slave = args[:slave] unless @slave
+        req(:required => [:master, :slave],
+            :argsObject => args)
+        args.delete(:sql) if args.has_key?(:sql)
+        dbh = Mysqladmin::Exec.new(:connectionName => @slave, :sql => "SHOW SLAVE STATUS")
+        dbh.go
+        res = dbh.fetch_hash
+        @sbm = res["Seconds_Behind_Master"]
+        @io = res["Slave_IO_Running"]
+        @sql = res["Slave_SQL_Running"]
+        
+      end
+  
+      def repStatusPool(args={})
+        
+      end
+    end
+    
+    class Sync
+      def initialize(args={})
+        @master = args[:master] || nil
+        @slave = args[:slave] || nil
+      end
+      
+      def syncTable(args={})
+        
+      end
+      
+      def syncHost(args={})
+        
+      end
+      
+      def syncPool(args={})
+        
+      end
+    end
   end
 end
