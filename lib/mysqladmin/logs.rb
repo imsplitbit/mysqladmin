af3be61a79a5bc8c9aa13a8837c488fc72f77814
Daniel Salinas <imsplitbit@gmail.com>
2009-02-23 19:31:16 +0000

    created a log finder for locally connected mysql hosts.  This will give us the ability to use the library to examine logs by position to determine the full query in cases where the log files/slave status is truncated.

--- lib/mysqladmin/logs.rb
+++ lib/mysqladmin/logs.rb
@@ -4,7 +4,8 @@ module Mysqladmin
     include Mysqladmin::Arguments
     include Mysqladmin::ServerInfo
     
-    attr_accessor :time, :foo
+    attr_accessor :time
+    attr_reader :binLogs, :slowLog, :genLog, :relayLog
     
     def initialize(args={})
       req(:required => [:connectionName], :argsObject => args)
@@ -12,20 +13,45 @@ module Mysqladmin
       @binLogs = {}
       @slowLog = nil
       @genLog = nil
-      @relayLog = nil
+      @relayLogs = {}
       @vars = serverVariables(:connectionName => args[:connectionName], :type => "variables")
       @status = serverVariables(:connectionName => args[:connectionName], :type => "status")
-    end
-    
-    def findLogs(args = {})
-      
+      parseMyCnf
     end
     
     def parseMyCnf
       myCnfFiles = ["/etc/my.cnf", "#{@vars[:datadir]}/my.cnf"]
       myCnfFiles.each do |file|
-        if File.exist?
+        if line.include?("!include")
+          myCnfFiles << line.split(" ")[1]
+        end
+      end
+      myCnfFiles.each do |file|
+        if File.exist?(file)
+          File.readlines(file).each do |line|
+            var = line.split("=")[0].gsub("-", "_")
+            val = line.split("=")[1]
+            if var[/log_bin/]
+              Dir.glob("#{val}*").delete_if{|x| x[/index$/]}.each do |binLog|
+                binLogName = File.basename(binlog)
+                @binLogs[binLogName] = {}
+                @binLogs[binLogName][:path] = binLog
+              end
+            elsif var[/relay_log/]
+              Dir.glob("#{val}*").delete_if{|x| x[/index/]}.each do |relayLog|
+                relayLogName = File.basename(relayLog)
+                @relayLogs[relayLog] = {}
+                @relayLogs[relayLog][:path] = relayLog
+              end
+            elsif var[/log_slow_queries/]
+              @slowLog = val
+            elsif var[/^log$/]
+              @genLog = val
+            end
+          end
+        end
       end
     end
+    
   end
 end
