b05cda8aa665e979477a2c7f35a68f49875f8c83
Daniel Salinas <imsplitbit@gmail.com>
2009-07-01 14:14:25 +0000

    fixed names in showgrants example

--- examples/showgrants.rb
+++ examples/showgrants.rb
@@ -13,14 +13,14 @@ Mysqladmin::Pool.add_connection( :host => "localhost",
                                  :user => "root",
                                  :password => "password",
                                  :connection_name => "local1")
-rootlocal = Mysqladmin::User.new(:user => "341448_wpsb",
+rootlocal = Mysqladmin::User.new(:user => "test",
                                  :src_connection => "local1")
 rootlocal.get_grants
 rootlocal.conv_grants_to_revokes
 #rootlocal.set_revokes(:connection_name => "local1")
 rootlocal.get_grants
 pp rootlocal
-rootlocal.save(:file_name => "341448user.yml")
-new_obj = rootlocal.load(:file_name => "341448user.yml")
+rootlocal.save(:file_name => "test.yml")
+new_obj = rootlocal.load(:file_name => "test.yml")
 pp new_obj
 Mysqladmin::Pool.close_all_connections
